246b712d9c9d4266830ba0befc32c16aebaa8fc3
OliverCadman <o.cadman@live.co.uk>
2021-07-12 15:38:52 +0100

    Created array of objects containing names of notes, and relative frequencies

---
+++ assets/js/audio.js
@@ -0,0 +1,15 @@
+const tones = [
+  { note: "C4", frequency: 261.63 },
+  { note: "C#4", frequency: 277.18 },
+  { note: "D4", frequency: 293.66 },
+  { note: "D#4", frequency: 311.13 },
+  { note: "E4", frequency: 329.63 },
+  { note: "F4", frequency: 349.23 },
+  { note: "F#4", frequency: 369.99 },
+  { note: "G4", frequency: 392 },
+  { note: "G#4", frequency: 415.3 },
+  { note: "A4", frequency: 440 },
+  { note: "A#4", frequency: 466.16 },
+  { note: "B4", frequency: 493.88 },
+  { note: "C5", frequency: 523.25 },
+];
