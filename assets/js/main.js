f391a5b60a9d305ca5357ded924d6cf61378bf73
OliverCadman <o.cadman@live.co.uk>
2021-07-25 11:06:18 +0100

    Relocated object array of intervals from audio.js to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,70 @@ let answerList = [
   "Major Sixth",
   "Minor Seventh",
   "Major Seventh",
-  "Octave"
+  "Octave",
+];
+
+const intervals = [
+  {
+    cOctave: {
+      frequency1: 261.63,
+      frequency2: 523.25,
+    },
+
+    cMinorSecond: {
+      frequency1: 261.63,
+      frequency2: 277.18,
+    },
+
+    cMajorSecond: {
+      frequency1: 261.63,
+      frequency2: 293.66,
+    },
+
+    cMinorThird: {
+      frequency1: 261.63,
+      frequency2: 311.13,
+    },
+
+    cMajorThird: {
+      frequency1: 261.63,
+      frequency2: 326.63,
+    },
+
+    cPerfectFourth: {
+      frequency1: 261.63,
+      frequency2: 349.23,
+    },
+
+    cAugmentedFourth: {
+      frequency1: 261.63,
+      frequency2: 369.99,
+    },
+
+    cPerfectFifth: {
+      frequency1: 261.63,
+      frequency2: 392,
+    },
+
+    cMinorSixth: {
+      frequency1: 261.63,
+      frequency2: 415.3,
+    },
+
+    cMajorSixth: {
+      frequency1: 261.63,
+      frequency2: 440,
+    },
+
+    cMinorSeventh: {
+      frequency1: 261.63,
+      frequency2: 466.16,
+    },
+
+    cMajorSeventh: {
+      frequency1: 261.63,
+      frequency2: 493.88,
+    },
+  },
 ];
 
