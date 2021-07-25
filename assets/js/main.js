2d8156cef7644a6c142ae3b3fbd68e3fa5888587
OliverCadman <o.cadman@live.co.uk>
2021-07-25 10:46:51 +0100

    Initialized variables required to play game, and added array of answers in string format

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let interval1;
+
+let interval2;
+
+let userAnswer;
+
+let correctAnswer;
+
+let correctAnswersRemaining = 20;
+
+let livesRemaining = 3;
+
+let answerList = [
+  "Minor Second",
+  "Major Second",
+  "Minor Third",
+  "Major Third",
+  "Perfect Fourth",
+  "Augmented Fourth",
+  "Perfect Fifth",
+  "Minor Sixth",
+  "Major Sixth",
+  "Minor Seventh",
+  "Major Seventh",
+  "Octave"
+];
+
