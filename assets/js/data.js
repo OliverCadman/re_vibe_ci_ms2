60adec1ff9d9e0a765806e0c97f690b2ef57e198
OliverCadman <o.cadman@live.co.uk>
2021-07-29 16:32:16 +0100

    Moved answerList and intervalist into dedicated data.js file, linked in index.html

---
+++ assets/js/data.js
@@ -0,0 +1,77 @@
+let answerList = [
+  "Minor Second",
+  "Major Second",
+  "Minor Third",
+  "Major Third",
+  "Perfect Fourth",
+  "Augmented Fourth",
+  "Perfect Fifth",
+  "Minor Sixth",
+  "Major Sixth",
+  "Minor Seventh",
+  "Major Seventh",
+  "Octave",
+];
+
+const intervalList = [
+  {
+    interval: "Octave",
+    frequency1: 261.63,
+    frequency2: 523.25,
+  },
+  {
+    interval: "Minor Second",
+    frequency1: 261.63,
+    frequency2: 277.18,
+  },
+  {
+    interval: "Major Second",
+    frequency1: 261.63,
+    frequency2: 293.66,
+  },
+  {
+    interval: "Minor Third",
+    frequency1: 261.63,
+    frequency2: 311.13,
+  },
+  {
+    interval: "Major Third",
+    frequency1: 261.63,
+    frequency2: 326.63,
+  },
+  {
+    interval: "Perfect Fourth",
+    frequency1: 261.63,
+    frequency2: 349.23,
+  },
+  {
+    interval: "Augmented Fourth",
+    frequency1: 261.63,
+    frequency2: 369.99,
+  },
+  {
+    interval: "Perfect Fifth",
+    frequency1: 261.63,
+    frequency2: 392,
+  },
+  {
+    interval: "Minor Sixth",
+    frequency1: 261.63,
+    frequency2: 415.3,
+  },
+  {
+    interval: "Major Sixth",
+    frequency1: 261.63,
+    frequency2: 440,
+  },
+  {
+    interval: "Minor Seventh",
+    frequency1: 261.63,
+    frequency2: 466.16,
+  },
+  {
+    interval: "Major Seventh",
+    frequency1: 261.63,
+    frequency2: 493.88,
+  },
+];
