cb5a63dffff691b597afe5ac4c0aa6e71e2ae677
OliverCadman <o.cadman@live.co.uk>
2021-08-03 18:34:20 +0100

    Pushing data.js to repository for deploying

--- assets/js/data.js
+++ assets/js/data.js
@@ -17,61 +17,121 @@ const intervalList = [
   {
     interval: "Octave",
     frequency1: 261.63,
-    frequency2: 523.25,
+    frequency2: 523.25
+  },
+  {
+    interval: "Octave",
+    frequency1: 293.66,
+    frequency2: 587.33
   },
   {
     interval: "Minor Second",
     frequency1: 261.63,
-    frequency2: 277.18,
+    frequency2: 277.18
+  },
+  {
+    interval: "Minor Second",
+    frequency1: 293.66,
+    frequency2: 311.13
   },
   {
     interval: "Major Second",
     frequency1: 261.63,
-    frequency2: 293.66,
+    frequency2: 293.66
+  },
+  {
+    interval: "Major Second",
+    frequency1: 293.66,
+    frequency2: 326.63
   },
   {
     interval: "Minor Third",
     frequency1: 261.63,
-    frequency2: 311.13,
+    frequency2: 311.13
+  },
+  {
+    interval: "Minor Third",
+    frequency1: 293.66,
+    frequency2: 349.23
   },
   {
     interval: "Major Third",
     frequency1: 261.63,
-    frequency2: 326.63,
+    frequency2: 326.63
+  },
+  {
+    interval: "Major Third",
+    frequency1: 293.66,
+    frequency2: 369.99
   },
   {
     interval: "Perfect Fourth",
     frequency1: 261.63,
-    frequency2: 349.23,
+    frequency2: 349.23
+  },
+  {
+    interval: "Perfect Fourth",
+    frequency1: 293.66,
+    frequency2: 392
   },
   {
     interval: "Augmented Fourth",
     frequency1: 261.63,
-    frequency2: 369.99,
+    frequency2: 369.99
+  },
+  {
+    interval: "Augmented Fourth",
+    frequency1: 293.66,
+    frequency2: 415.3
   },
   {
     interval: "Perfect Fifth",
     frequency1: 261.63,
-    frequency2: 392,
+    frequency2: 392
+  },
+  {
+    interval: "Perfect Fifth",
+    frequency1: 293.66,
+    frequency2: 440
   },
   {
     interval: "Minor Sixth",
     frequency1: 261.63,
-    frequency2: 415.3,
+    frequency2: 415.3
+  },
+  {
+    interval: "Minor Sixth",
+    frequency1: 293.66,
+    frequency2: 466.16
   },
   {
     interval: "Major Sixth",
     frequency1: 261.63,
-    frequency2: 440,
+    frequency2: 440
+  },
+  {
+    interval: "Major Sixth",
+    frequency1: 293.66,
+    frequency2: 493.88
   },
   {
     interval: "Minor Seventh",
     frequency1: 261.63,
-    frequency2: 466.16,
+    frequency2: 466.16
+  },
+  {
+    interval: "Minor Seventh",
+    frequency1: 293.66,
+    frequency2: 523.25
   },
   {
     interval: "Major Seventh",
     frequency1: 261.63,
-    frequency2: 493.88,
+    frequency2: 493.88
   },
+  {
+    interval: "Major Seventh",
+    frequency1: 293.66,
+    frequency2: 554.37
+  }
 ];
