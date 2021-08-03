c4cf2bf8d783c27ed801862135ff990511819d68
OliverCadman <o.cadman@live.co.uk>
2021-08-03 10:30:29 +0100

    Created new envelope. Both noteOscillatorOne and noteOsilattorTwo have dedicated envelopes (envelopeOne and envelopeTwo, respectively)

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -4,86 +4,6 @@
 
 // Assign constant variable to an object array containing a single octave containing 13 notes, with their relative frequencies.
 
-const tones = [
-  { note: "C4", frequency: 261.63 },
-  { note: "C#4", frequency: 277.18 },
-  { note: "D4", frequency: 293.66 },
-  { note: "D#4", frequency: 311.13 },
-  { note: "E4", frequency: 329.63 },
-  { note: "F4", frequency: 349.23 },
-  { note: "F#4", frequency: 369.99 },
-  { note: "G4", frequency: 392 },
-  { note: "G#4", frequency: 415.3 },
-  { note: "A4", frequency: 440 },
-  { note: "A#4", frequency: 466.16 },
-  { note: "B4", frequency: 493.88 },
-  { note: "C5", frequency: 523.25 },
-];
-
-const intervals = [
-  {
-    cOctave: {
-      frequency1: 261.63,
-      frequency2: 523.25,
-    },
-
-    cMinorSecond: {
-      frequency1: 261.63,
-      frequency2: 277.18,
-    },
-
-    cMajorSecond: {
-      frequency1: 261.63,
-      frequency2: 293.66,
-    },
-
-    cMinorThird: {
-      frequency1: 261.63,
-      frequency2: 311.13,
-    },
-
-    cMajorThird: {
-      frequency1: 261.63,
-      frequency2: 326.63,
-    },
-
-    cPerfectFourth: {
-      frequency1: 261.63,
-      frequency2: 349.23,
-    },
-
-    cAugmentedFourth: {
-      frequency1: 261.63,
-      frequency2: 369.99,
-    },
-
-    cPerfectFifth: {
-      frequency1: 261.63,
-      frequency2: 392,
-    },
-
-    cMinorSixth: {
-      frequency1: 261.63,
-      frequency2: 415.3,
-    },
-
-    cMajorSixth: {
-      frequency1: 261.63,
-      frequency2: 440,
-    },
-
-    cMinorSeventh: {
-      frequency1: 261.63,
-      frequency2: 466.16,
-    },
-
-    cMajorSeventh: {
-      frequency1: 261.63,
-      frequency2: 493.88,
-    },
-  },
-];
-
 // URL to source of Impulse Response, to be used to create convolution reverb.
 const impulseURL = "../assets/impulse-response/impulse-response-2.wav";
 
@@ -128,27 +48,40 @@ function createTones(interval1, interval2) {
   const releaseTime = 1;
   const now = audioContext.currentTime;
 
-  const envelope = audioContext.createGain(); // Creates gain node to use to build envelope curve
-  envelope.gain.setValueAtTime(0, 0);
+  // envelopeOne assigned to noteOscillatorOne
+  const envelopeOne = audioContext.createGain(); // Creates gain node to use to build envelope curve
+  envelopeOne.gain.setValueAtTime(0, 0);
 
   // First parameter is the value that the gain will change to. Second parameter represents the time taken to reach the value from last instance of AudioContext ('now').
-  envelope.gain.linearRampToValueAtTime(0.5, now + attackTime);
-  envelope.gain.linearRampToValueAtTime(
+  envelopeOne.gain.linearRampToValueAtTime(0.5, now + attackTime);
+  envelopeOne.gain.linearRampToValueAtTime(
     sustainLevel,
     now + attackTime + decayTime
   );
-  envelope.gain.setValueAtTime(sustainLevel, 1 - releaseTime);
-  envelope.gain.linearRampToValueAtTime(0, now + 2);
+  envelopeOne.gain.setValueAtTime(sustainLevel, 1 - releaseTime);
+  envelopeOne.gain.linearRampToValueAtTime(0, now + 2);
+
+  // envelopeTwo assigned to noteOscillatorTwo
+  const envelopeTwo = audioContext.createGain();
+  
+   envelopeTwo.gain.linearRampToValueAtTime(0.5, now + attackTime);
+   envelopeTwo.gain.linearRampToValueAtTime(
+     sustainLevel,
+     now + attackTime + decayTime
+   );
+   envelopeTwo.gain.setValueAtTime(sustainLevel, 1 - releaseTime);
+   envelopeTwo.gain.linearRampToValueAtTime(0, now + 2);
+  
 
   // Hooks up the oscillators to audio processors, then to masterGainControl
-  noteOscillatorOne.connect(envelope);
-  envelope.connect(sineGain);
+  noteOscillatorOne.connect(envelopeOne);
+  envelopeOne.connect(sineGain);
   sineGain.connect(masterGainControl);
   noteOscillatorOne.start(now);
   noteOscillatorOne.stop(now + 2);
 
-  noteOscillatorTwo.connect(envelope);
-  envelope.connect(masterGainControl);
+  noteOscillatorTwo.connect(envelopeTwo);
+  envelopeTwo.connect(masterGainControl);
   noteOscillatorTwo.start(now + 1);
   noteOscillatorTwo.stop(now + 2);
 
