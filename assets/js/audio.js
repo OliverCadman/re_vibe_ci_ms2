f0613fc02ca264c6385ed3e5a124bf53e8136f25
OliverCadman <o.cadman@live.co.uk>
2021-08-04 19:00:26 +0100

    Assigned variable synth to tone.js polysynth, with triggerAttackRelease property. Frequency parameters passed in to get voices

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -25,5 +25,26 @@ function createInterval(frequency1, frequency2) {
 }
 
 function createChord(frequency1, frequency2, frequency3, frequency4) {
- 
+  const envelope = new Tone.AmplitudeEnvelope({
+    attack: 0.8,
+    decay: 0.2,
+    sustain: 1.0,
+    release: 0.8,
+  });
+
+  const gain = new Tone.Gain(0.5);
+
+  const synth = new Tone.PolySynth(Tone.Synth).connect(envelope);
+
+  synth.set({
+    waveform: "sine",
+  });
+  synth.triggerAttackRelease(
+    [frequency1, frequency2, frequency3, frequency4],
+    1
+  );
+
+  synth.chain(gain, filter, Tone.Destination);
+
+  Tone.start();
 }
