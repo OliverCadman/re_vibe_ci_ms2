78c9cf6628c7f46c17dba0811ff94b8786e9d1ec
OliverCadman <o.cadman@live.co.uk>
2021-08-15 23:02:37 +0100

    created function validateForm, with variables assigned to inputs, textareas and error-message div

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
     })
 })
 
+/* validateForm provides a custom error message
+if the user inputs invalid values.
+Code referenced from YouTube tutorial:
+Title: Contact Us Form Validation Using Javascript | Form Validation In Javascript
+Link: https://www.youtube.com/watch?v=WY4rvU4ImgE */
+function validateForm(contactForm) {
 
+  let nameInput = document.getElementById("name").value
+  let emailInput = document.getElementById("email-address").value
+  let questionsAreaInput = document.getElementById("questions-feedback").value
+
+  let errorMessage = document.getElementsByClassName('error-message');
+}
