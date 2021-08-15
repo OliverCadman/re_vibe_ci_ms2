613381a189cea98b4634687d813b6322faa1ee00
OliverCadman <o.cadman@live.co.uk>
2021-08-15 23:05:34 +0100

    set innerHTML of errorMessage variable with error message, using if else statements for each variable assigned to inputs and textarea

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -17,4 +17,37 @@ function validateForm(contactForm) {
   let questionsAreaInput = document.getElementById("questions-feedback").value
 
   let errorMessage = document.getElementsByClassName('error-message');
-}
+
+  if (nameInput.length < 2 ) {
+        errorMessage[0].style.padding = "5px"
+        errorMessage[0].innerHTML = "Please enter your full name"
+        return false;
+  } else {
+      // Clears error message once user input is valid
+      errorMessage[0].innerHTML = ''
+      errorMessage[0].style.padding = "0px";
+  };
+
+  if (emailInput.indexOf("@") === -1) {
+      errorMessage[1].style.padding = "5px"
+      errorMessage[1].innerHTML = `Please enter a valid email address`
+      return false;
+  } else {
+      errorMessage[1].style.padding = "0px";
+      errorMessage[1].innerHTML = '';
+  };
+
+  if (questionsAreaInput.length < 50) {
+      errorMessage[2].style.padding = "5px"
+      errorMessage[2].innerHTML = `Please enter more than 50 characters`
+      return false;
+  } else {
+      errorMessage[2].style.padding = "0px";
+      errorMessage[2].innerHTML = ''
+  };
+
+  // sendEmail invoked once all user input is validated
+  return sendEmail(contactForm);
+};
+
+
