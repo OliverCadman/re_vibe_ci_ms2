a2885542054a96b0e1f2bf5e90533bc65501ae83
OliverCadman <o.cadman@live.co.uk>
2021-08-16 17:41:55 +0100

    Added comment 'jshint esversion: 6'

--- assets/js/animate-hamburger.js
+++ assets/js/animate-hamburger.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 /* Animate hamburger icon on mobile devices by adding CSS class 'open'
 when hamburger icon is clicked. Code referenced from YouTube tutorial:
 Title: Animated Hamburger Menu Tutorial
@@ -6,12 +8,12 @@ Link: https://www.youtube.com/watch?v=dIyVTjJAkLw&t=336s */
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 let isMenuOpen = false;
 
-hamburgerIcon.addEventListener("click", function() {
-    if (!isMenuOpen) {
-        isMenuOpen = true;
-        hamburgerIcon.classList.add('open');
-    } else {
-        isMenuOpen = false;
-        hamburgerIcon.classList.remove('open');
-    }
+hamburgerIcon.addEventListener("click", function () {
+  if (!isMenuOpen) {
+    isMenuOpen = true;
+    hamburgerIcon.classList.add("open");
+  } else {
+    isMenuOpen = false;
+    hamburgerIcon.classList.remove("open");
+  }
 });
