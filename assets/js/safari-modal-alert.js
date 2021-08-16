04d70b1528ea100a3f1825c75d3b1ef8622b05cb
OliverCadman <o.cadman@live.co.uk>
2021-08-16 17:53:24 +0100

    Added safari-modal-alert js file back into the mix

---
+++ assets/js/safari-modal-alert.js
@@ -0,0 +1,21 @@
+/*jshint esversion: 6 */
+
+/* Displays modal on page load, to alert visitors using iOS Safari
+to click speaker icon once to hear the sounds. A temporary workaround to
+deal with bug resulting in Safari browsers suspending the Audio Context State. */
+
+/**
+ * Session storage code ensures the modal is displayed only upon the 
+first visit to page, and will remain closed until the visitor closes their
+browser window.
+Code referenced from https://stackoverflow.com/questions/59660094/popup-modal-by-browser-session 
+ *  */
+$(document).ready(function () {
+  if (!sessionStorage.getItem("firstVisit")) {
+    $("#safari-alert-modal").modal("show");
+  }
+  $(".close-modal-btn").on("click", function () {
+    $("#safari-alert-modal").modal("hide");
+    sessionStorage.setItem("firstVisit", true);
+  });
+});
