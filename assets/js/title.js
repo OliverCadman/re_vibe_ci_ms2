ee61f9d43f10fa99a56743f86dff6e47caa25ee7
OliverCadman <o.cadman@live.co.uk>
2021-08-16 17:43:06 +0100

    Added comment 'jshint esversion: 6'

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,20 +1,15 @@
+/*jshint esversion: 6 */
+
 $(document).ready(function () {
   setTimeout(function () {
     $("div.hidden").fadeTo(1000, 1);
   }, 2000);
 });
 
-new Vivus("ear-drawing", { duration: 150 });
+const earAnimation = new Vivus("ear-drawing", { duration: 150 });
 
-new Vivus("logo-heading", {
+const logoAnimation = new Vivus("logo-heading", {
   duration: 150,
   type: "sync",
   animTimingFunction: Vivus.EASE_OUT,
 });
-
-function hideTitlePage() {
-  $("#hide-container").fadeTo(400, 0);
-  $(".game-container")
-    .removeClass("game-container")
-    .addClass("game-container-show");
-}
